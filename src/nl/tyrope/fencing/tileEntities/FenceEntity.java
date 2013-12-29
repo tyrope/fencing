cbafc9c7096f9b29affecacfd5181a76a8825920
Dimitri Molenaars <Tyrope@TyRope.nl>
2013-12-29 21:12:14 +0100

    Remove unneeded debug prints, label the useful ones.

--- src/nl/tyrope/fencing/tileEntities/FenceEntity.java
+++ src/nl/tyrope/fencing/tileEntities/FenceEntity.java
@@ -15,10 +15,12 @@ public class FenceEntity extends TileEntity {
 	boolean[] canConnectTo = { false, false, false, false };
 
 	public int getModel() {
-		return modelType;
+		// System.out.println(" Model: " + this.modelType); // DEBUG
+		return this.modelType;
 	}
 
 	public float[] getRotation() {
+		// TODO Turn the model.
 		return new float[] { 0.0f, 0.0f, 1.0f };
 	}
 
@@ -45,29 +47,23 @@ public class FenceEntity extends TileEntity {
 				// Air block or some sort of error... Don't connect to it, it's
 				// probably poisonous.
 				canConnectTo[i] = false;
-				System.out.println(i + "null"); // DEBUG
 			} else if (Neighbors[i].isOpaqueCube()) {
 				// Neighbor is a full block.
 				canConnectTo[i] = true;
-				System.out.println(i + "isOpaqueCube"); // DEBUG
 			} else if (Neighbors[i] instanceof FenceBlock) {
 				// Neighbor is a Fencing fence.
 				canConnectTo[i] = true;
-				System.out.println(i + "FenceBlock"); // DEBUG
 			} else if (Neighbors[i].blockID == Block.fence.blockID
 					|| Neighbors[i] instanceof BlockPane) {
 				// Neighbor is a vanilla fence or iron bars/glass panes
 				canConnectTo[i] = true;
-				System.out.println(i + "Block.fence.blockID or BlockPane"); // DEBUG
 			} else if (Neighbors[i] instanceof BlockStairs) {
 				// Neighbor is a stairs block,
 				// TODO see if we're against a solid side somehow.
 				canConnectTo[i] = false;
-				System.out.println(i + "BlockStairs"); // DEBUG
 			} else {
 				// Don't connect.
 				canConnectTo[i] = false;
-				System.out.println(i + "Other"); // DEBUG
 			}
 			if (canConnectTo[i]) {
 				connectedSides++;
@@ -87,6 +83,8 @@ public class FenceEntity extends TileEntity {
 			// X-section
 			this.modelType = 3;
 		}
-		System.out.println(connectedSides + " " + this.modelType); // DEBUG FIXME Doesn't actually save.
+		// FIXME Doesn't actually save.
+		System.out.println("Sides connected:" + connectedSides + " Model Type:"
+				+ this.modelType); // DEBUG
 	}
 }
