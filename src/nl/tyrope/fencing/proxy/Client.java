b5095a75856847185330fbfe8f909cc652e613f9
Dimitri Molenaars <Tyrope@TyRope.nl>
2013-12-30 12:31:34 +0100

    Client proxy, Y U NO SAVE?!

--- src/nl/tyrope/fencing/proxy/Client.java
+++ src/nl/tyrope/fencing/proxy/Client.java
@@ -1,14 +1,20 @@
 package nl.tyrope.fencing.proxy;
 
-import nl.tyrope.fencing.render.FenceRenderer;
-import nl.tyrope.fencing.tileEntities.FenceEntity;
+import nl.tyrope.fencing.blocks.FenceBlock;
+import nl.tyrope.fencing.renderer.FenceBlockRenderer;
 import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
+import cpw.mods.fml.client.registry.RenderingRegistry;
 
 public class Client extends Common {
 
 	@Override
-	public void registerRenderers() {
-		ClientRegistry.bindTileEntitySpecialRenderer(FenceEntity.class,
-				new FenceRenderer());
+	public void registerRenderers(FenceBlock fenceBlock) {
+		int id = RenderingRegistry.getNextAvailableRenderId();
+		ISimpleBlockRenderingHandler fenceBlockRenderer = new FenceBlockRenderer(
+				id);
+		fenceBlock.renderId = id;
+		RenderingRegistry.registerBlockHandler(fenceBlock.getRenderType(),
+				fenceBlockRenderer);
 	}
 }
