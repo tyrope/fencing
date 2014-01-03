7f960abe685d842b2e8e860150fb762cfd8f9224
Dimitri Molenaars <Tyrope@TyRope.nl>
2014-01-03 03:46:16 +0100

    Changing version number details around. (Build ID is now added by gradle)

--- src/main/java/nl/tyrope/fencing/Refs.java
+++ src/main/java/nl/tyrope/fencing/Refs.java
@@ -3,7 +3,7 @@ package nl.tyrope.fencing;
 public class Refs {
 	// Forge needs these.
 	public final static String MODID = "fencing";
-	public final static String VERSION = "0.1.0.1";
+	public final static String VERSION = "0.1.0";
 
 	// Item IDs for future use.
 	public static int PoleID;
