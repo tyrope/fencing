f4abed20eff2852deef5faee150c011533f58f36
Dimitri Molenaars <Tyrope@TyRope.nl>
2014-01-17 22:22:35 +0100

    Pushing to release became a headache, put build scripts on this branch as well.

---
+++ build.sh
@@ -0,0 +1,11 @@
+echo "        STATUS: Preparing build file"
+sed 's/BUILDNO/'$BUILD_NUMBER'/' <build.template >build.gradle
+
+echo "        STATUS: Building"
+./gradlew build
+
+echo "        STATUS: Moving release file"
+mv -uf build/libs/* fencing-nightly.jar
+
+echo "        STATUS: Cleanup"
+rm -fr build
