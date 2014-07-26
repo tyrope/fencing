6ef72668f1ab2954478999d02e81aba201cb2ad6
Antoine <antoine.lucas.33@gmail.com>
2014-07-26 21:22:16 +0200

    Replace empty hitbox, juste return null

--- src/main/java/nl/tyrope/fencing/blocks/FenceBlock.java
+++ src/main/java/nl/tyrope/fencing/blocks/FenceBlock.java
@@ -178,8 +178,7 @@ public class FenceBlock extends BlockContainer {
 			int y, int z) {
 		if (world.getBlockMetadata(x, y, z) == Refs.MetaValues.FenceCut
 				&& world.getBlock(x, y, z) == Refs.ItemsBlocks.Fence) {
-			// XXX Hitbox at [0,-1, 0] might cause issues later.
-			return AxisAlignedBB.getAABBPool().getAABB(0, -1, 0, 0, -1, 0);
+			return null;
 		} else {
 			return getHitBox(world, x, y, z).expand(0, 0.3f, 0);
 		}
